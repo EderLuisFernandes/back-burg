38c28d2722650fcfd4eeaafe005c79b42e4c8169
Ederl010203 <eder.luis.1996@hotmail.com>
2024-04-11 11:53:11 -0300

    bakc-burg

---
+++ src/database/migrations/20240411144245-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  // o up serve para adiciona no meu banco de dados
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("user", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  // ele desfaz tudo que foi fei pelo up
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("user");
+  },
+};
